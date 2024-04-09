78daf80b0af48c8396026ccf1a0ddfc88d93527f
vvnn1_ <137418235@qq.com>
2024-04-09 13:22:15 +0800

    [build] 添加ORM框架

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.alibaba:druid-spring-boot-starter:1.1.10")
     implementation("mysql:mysql-connector-java:8.0.21")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
+    implementation("tk.mybatis:mapper-spring-boot-starter:4.3.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("junit:junit")
 }
