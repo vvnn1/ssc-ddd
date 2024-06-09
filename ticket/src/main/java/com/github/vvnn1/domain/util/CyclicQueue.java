943475b8499906a2f6554a6b49db775194a6b078
vvnn1_ <137418235@qq.com>
2024-06-09 19:04:14 +0800

    refactor: 修改循环队列实现

--- ticket/src/main/java/com/github/vvnn1/domain/util/CyclicQueue.java
+++ ticket/src/main/java/com/github/vvnn1/domain/util/CyclicQueue.java
@@ -6,41 +6,18 @@ import java.util.*;
  * @author vvnn1
  * @since 2024/5/25 14:36
  */
-public class CyclicQueue<T> extends AbstractQueue<T> {
-    private final Queue<T> queue;
+public class CyclicQueue<T> extends LinkedList<T> {
     private final int initialCapacity;
 
     public CyclicQueue(int initialCapacity) {
-        this.queue = new LinkedList<>();
         this.initialCapacity = initialCapacity;
     }
 
     @Override
-    public Iterator<T> iterator() {
-        return queue.iterator();
-    }
-
-    @Override
-    public int size() {
-        return queue.size();
-    }
-
-    @Override
     public boolean offer(T t) {
-        if (queue.size() == initialCapacity){
+        if (size() == initialCapacity){
             return false;
         }
-
-        return queue.offer(t);
-    }
-
-    @Override
-    public T poll() {
-        return queue.poll();
-    }
-
-    @Override
-    public T peek() {
-        return queue.peek();
+        return super.offer(t);
     }
 }
